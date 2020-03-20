47e09d236bbac6b95755be226ef7909fd8683a42
LEEjaeHyun <swaa23@gmail.com>
2020-03-20 17:41:51 +0900

    2020.03.20 homework1 pix(3)

--- homework1-1.py
+++ homework1-1.py
@@ -1,10 +1,5 @@
 import sys
 str1, str2 = list((map(str, input().split(','))))  # 단어의 구분을 , 로 함
-
-if len(str1) != len(str2):
-    print('Fasle')  # 입력값 단어 길이가 다를 때
-    sys.exit()
-
 a = sorted(str1)
 b = sorted(str2)
 if a == b:
