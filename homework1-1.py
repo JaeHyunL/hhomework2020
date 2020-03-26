13338b14625d631c07b8afe290aedf9af1806041
LEEjaeHyun <swaa23@gmail.com>
2020-03-26 18:19:19 +0900

    homework fix (4)

--- homework1-1.py
+++ homework1-1.py
@@ -1,8 +1,5 @@
-import sys
-str1, str2 = list((map(str, input().split(','))))  # 단어의 구분을 , 로 함
+﻿str1, str2 = list((map(str, input().split(','))))  # 단어의 구분을 , 로 함
 a = sorted(str1)
 b = sorted(str2)
-if a == b:
-    print(True)
-else:
-    print(False)
+if a == b: print(True)
+else: print(False)
