5c08abfe1dff525a6f73217f83230dcd19c3205a
LEEjaeHyun <swaa23@gmail.com>
2020-06-15 14:56:42 +0900

    입력범위수정 ...

--- FinalTest/q1.py
+++ FinalTest/q1.py
@@ -1,8 +1,9 @@
 
 import random
 
+
 def gen(n):
-    gen_random = random.sample(range(1, 100000), n)
+    gen_random = random.sample(range(0, n), n)
 
     return gen_random
 
@@ -12,29 +13,29 @@ def sorted_random(a):
     if num <= 1:
 
         return a
-    
+
     mid = num // 2
     g1 = sorted_random(a[:mid])
     g2 = sorted_random(a[mid:])
     result = []
-    
+
     while g1 and g2:
         if g1[0] < g2[0]:
             result.append(g1.pop(0))
         else:
             result.append(g2.pop(0))
-    
+
     while g1:
         result.append(g1.pop(0))
-    
+
     while g2:
         result.append(g2.pop(0))
-    
+
     return result
 
 
 if __name__ == "__main__":
     n = int(input())
-    gen_random = gen(n) # 정렬되지 않은 랜덤값 
-    print(gen_random) 
-    print(sorted_random(gen_random)) # 내림차순 정렬된 랜덤값 
+    gen_random = gen(n)  # 정렬되지 않은 랜덤값
+    print(gen_random)
+    print(sorted_random(gen_random))  # 내림차순 정렬된 랜덤값
